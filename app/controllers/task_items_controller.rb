7a3a4e08baf84560212a6181bd8c43ef3df7e3e1
Amirol Ahmad <9w2pdf@gmail.com>
2014-01-11 02:24:23 +0800

    Write tests for task items

--- app/controllers/task_items_controller.rb
+++ app/controllers/task_items_controller.rb
@@ -1,4 +1,5 @@
 class TaskItemsController < ApplicationController
   def index
+  	@ksat_list = KsatList.find(params[:ksat_list_id])
   end
 end
--- spec/features/task_items/index.spec.rb
+++ spec/features/task_items/index.spec.rb
@@ -2,11 +2,37 @@ require 'spec_helper'
 
 describe "Viewing task items" do
 	let!(:ksat_list) { KsatList.create(title: "Proton", description: "Suprima S") }
-	it "displays no items when a task list is empty" do
+
+	def visit_ksat_list(list)
 		visit "/ksat_lists"
-		within "#ksat_list_#{ksat_list.id}" do
+		within "#ksat_list_#{list.id}" do
 			click_link "List Items"
 		end
-		expect(page).to have_content("TaskItems#index")
+	end
+
+	it "displays the title of the todo list" do
+		visit_ksat_list(ksat_list)
+		within("h1") do
+			expect(page).to have_content(ksat_list.title)
+		end
+	end
+
+	it "displays no items when a task list is empty" do
+		visit_ksat_list(ksat_list)
+		expect(page.all("ul.task_items li").size).to eq(0)
+	end
+
+	it "displays item content when a ksat list has items" do
+		ksat_list.task_items.create(content: "Proton")
+		ksat_list.task_items.create(content: "Perodua")
+
+		visit_ksat_list(ksat_list)
+
+		expect(page.all("ul.task_items li").size).to eq(2)
+
+		within "ul.task_items" do
+			expect(page).to have_content("Proton")
+			expect(page).to have_content("Perodua")
+		end
 	end
 end
