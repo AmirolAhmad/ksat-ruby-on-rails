f34f1def49838398d1c8a35741b12b05f90e1289
Amirol Ahmad <9w2pdf@gmail.com>
2014-01-11 00:57:55 +0800

    Add specs for deleting tasks

---
+++ spec/features/ksat_lists/destroy_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "Deleting task lists" do
+	let!(:ksat_list) { ksat_list = KsatList.create(title: "Proton", description: "Suprima S.") }
+
+	it "is successful when clicking the destroy link" do
+		visit "/ksat_lists"
+
+		within "#ksat_list_#{ksat_list.id}" do
+			click_link "Destroy"
+		end
+		expect(page).to_not have_content(ksat_list.title)
+		expect(KsatList.count).to eq(0)
+	end
+end
