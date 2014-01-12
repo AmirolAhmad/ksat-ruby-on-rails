8a5da07e713e47b235ceceaf34e35f30ed666df2
Amirol Ahmad <9w2pdf@gmail.com>
2014-01-12 22:06:39 +0800

    Clean up task mark completion view

--- spec/features/task_items/complete_spec.rb
+++ spec/features/task_items/complete_spec.rb
@@ -14,4 +14,22 @@ describe "Completing task items" do
 		expect(task_item.completed_at).to_not be_nil
 	end
 
+	context "with completed items" do
+	  let!(:completed_task_item) { ksat_list.task_items.create(content: "Proton", completed_at: 5.minutes.ago) }
+
+	  it "shows completed items as complete" do
+	  	visit_ksat_list ksat_list
+	  	within dom_id_for(completed_task_item) do
+	  		expect(page).to have_content(completed_task_item.completed_at)
+	  	end
+	  end
+
+	  it "does not give the option to mark complete" do
+	  	visit_ksat_list ksat_list
+	  	within dom_id_for(completed_task_item) do
+	  		expect(page).to_not have_content("Mark as Complete")
+	  	end
+	  end
+	end
+
 end
